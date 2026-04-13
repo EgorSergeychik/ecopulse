5ef0ecb48bbcaa622f3218046bdf3386aa264824
EgorSergeychik <sergeychike.egor@gmail.com>
2026-04-13 21:25:34 +0200

    feat: branding

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import AppLogoIcon from '@/components/AppLogoIcon.vue';
+import { useSidebar } from '@/components/ui/sidebar/utils';
+
+const { state } = useSidebar();
 </script>
 
 <template>
     <div
-        class="flex aspect-square size-8 items-center justify-center rounded-md bg-white overflow-hidden border border-border dark:border-transparent"
+        class="flex aspect-square size-8 shrink-0 items-center justify-center rounded-md bg-white overflow-hidden border border-border dark:border-transparent"
     >
         <AppLogoIcon class="size-5" />
     </div>
-    <div class="ml-1 grid flex-1 text-left text-sm">
+    <div
+        class="ml-1 grid flex-1 text-left text-sm"
+        v-if="state !== 'collapsed'"
+    >
         <span class="mb-0.5 truncate leading-tight font-semibold"
             >Ecopulse</span
         >
